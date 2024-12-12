badbf2d095a6a504ba5e2ae4c612af141420346c
dchernokur <5022436+dchernokur@users.noreply.github.com>
2024-12-12 19:15:20 +0200

    fix: add correct path to node_modules to resolve libraries for tw. (#15)
    Signed-off-by: dchernokur <5022436+dchernokur@users.noreply.github.com>

--- packages/fe-tw/tailwind.config.ts
+++ packages/fe-tw/tailwind.config.ts
@@ -2,8 +2,8 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
 	content: [
-		"node_modules/daisyui/dist/**/*.js",
-		"node_modules/react-daisyui/dist/**/*.js",
+		"../../node_modules/daisyui/dist/**/*.js",
+		"../../node_modules/react-daisyui/dist/**/*.js",
 		"./src/**/*.{js,ts,jsx,tsx,mdx}",
 	],
 	theme: {
