c785e52aecfb2466e80d822242cc576b7115d0a4
nadine.loepfe <nadine.loepfe@swirldslabs.com>
2025-01-30 16:04:48 +0100

    rm abi
    Signed-off-by: nadine.loepfe <nadine.loepfe@swirldslabs.com>

--- packages/fe-tw/src/components/Admin/AdminInfoPanel.tsx
+++ packages/fe-tw/src/components/Admin/AdminInfoPanel.tsx
@@ -5,12 +5,11 @@ export function AdminInfoPanel() {
     <div className="bg-purple-50 p-6 rounded-lg">
       <h2 className="text-2xl font-bold mb-4">What You Can Do</h2>
       <p className="text-sm sm:text-base text-gray-700">
-        This interface allows you to manage buildings by adding new ones to the platform,
-        and also add liquidity to their token pairs.
+        This interface allows the management of buildings by adding new ones to the platform, as well as liquidity management.
       </p>
       <p className="mt-4 text-sm sm:text-base text-gray-700">
-        Filling in the building form creates a new building (mocked or real).
-        Adding liquidity ensures there's an actual token pair for trading.
+        Filling in the building form creates a new building.
+        Adding liquidity ensures there's tokens for trading.
       </p>
     </div>
   );
