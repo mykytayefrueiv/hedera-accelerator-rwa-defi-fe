923e1dd3d5e3ca36fe7cfd838c5e0533ab0f228b
dchernokur <5022436+dchernokur@users.noreply.github.com>
2025-02-18 13:38:10 +0100

    chore: rename pinata url env variable to align with recent changes.
    Signed-off-by: dchernokur <5022436+dchernokur@users.noreply.github.com>

--- packages/fe-tw/src/utils/helpers.ts
+++ packages/fe-tw/src/utils/helpers.ts
@@ -1,2 +1,2 @@
 export const prepareIPFSfileURL = (ipfsHash: string) =>
-	`https://${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/${ipfsHash}`;
+	`https://${process.env.NEXT_PUBLIC_PINATA_GATEWAY_URL}/ipfs/${ipfsHash}`;
